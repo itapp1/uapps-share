431c48d52ca73397d37188b612ac874b0c3a16c9
dazelpro <dazelpro@gmail.com>
2020-10-05 09:33:30 +0700

    tes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 // const loginRoutes = require('./src/routes/router-login');
 
-app.use('/login', loginRoutes);
+// app.use('/login', loginRoutes);
 
 let PORT = 3000;
 app.listen(PORT, ()=>{
