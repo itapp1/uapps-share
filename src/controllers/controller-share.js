5b3967cca33de624389cb0d9bcbd308bfcd721b3
dazelpro <dazelpro@gmail.com>
2020-10-05 10:49:24 +0700

    + meta refresh

--- src/controllers/controller-share.js
+++ src/controllers/controller-share.js
@@ -4,7 +4,7 @@ module.exports ={
        res.render('shareWithoutDB',{
             textDisplay: 'Please wait ...',
             url: 'http://localhost:3000/',
-            urlRedirect : ''
+            urlRedirect : 'https://sales.uapps.id/products/detail/100473'
        });
     },
     shareWithDB(req,res){
