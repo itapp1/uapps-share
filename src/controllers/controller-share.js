dbd81f1fb89276e25268511776f1a178a6cd9d6b
dazelpro <dazelpro@gmail.com>
2020-10-05 11:07:47 +0700

    + variable

--- src/controllers/controller-share.js
+++ src/controllers/controller-share.js
@@ -7,6 +7,8 @@ module.exports ={
             url: 'http://localhost:3000/',
             urlRedirect : `https://sales.uapps.id/products/detail/${id}`,
             urlImage : `https://sales.uapps.id/assets/image/SENTRALTUKANG/detail/${id}[0].png`,
+            titleProduct : 'FOX Lem Banteng KW 1',
+            descriptionProduct : 'Fox Lem Banteng Kualitas KW 1 Harga boleh di adu.'
         });
     },
     shareWithDB(req,res){
