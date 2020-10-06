8e38d14e5026573ed8f0f7feabbc413bff3bfda9
dazelpro <dazelpro@gmail.com>
2020-10-06 11:51:51 +0700

    tes connection

--- src/controllers/controller-share.js
+++ src/controllers/controller-share.js
@@ -1,5 +1,5 @@
-let ConnectionPool  = require('tedious-connection-pool');
-let Request         = require('tedious').Request;
+// let ConnectionPool  = require('tedious-connection-pool');
+// let Request         = require('tedious').Request;
 
 require('dotenv').config();
 const { 
@@ -30,23 +30,22 @@ if (NODE_ENV === 'production') {
     urlImage = URL_IMAGE_DEV
 }
 
-SQL_SERVER = {
-    userName: USER_SERVER,
-    user: USER_SERVER,
-    password: PASSWORD_SERVER,
-    server: SQL_DATABASE_SERVER
-}
+// SQL_SERVER = {
+//     userName: USER_SERVER,
+//     password: PASSWORD_SERVER,
+//     server: SQL_DATABASE_SERVER
+// }
 
-let poolConfig = {
-    min: 2,
-    max: 8,
-    log: false
-};
-let pool = new ConnectionPool(poolConfig, SQL_SERVER);
+// let poolConfig = {
+//     min: 2,
+//     max: 8,
+//     log: false
+// };
+// let pool = new ConnectionPool(poolConfig, SQL_SERVER);
 
-pool.on('error',(err)=> {
-    console.error(err);
-});
+// pool.on('error',(err)=> {
+//     console.error(err);
+// });
 
 
 module.exports ={
@@ -63,35 +62,88 @@ module.exports ={
     shareWithDB(req,res){
         let db = 'SENTRALTUKANG';
         let id = req.params.id;
-		let rows = [];
+        var Connection = require('tedious').Connection;
+        var config = {
+            userName: 'uAppsBackend',
+            password: 'Theodorus@.',
+            server: '172.18.29.11',
+            // When you connect to Azure SQL Database, you need these next options.
+            // options: {encrypt: true,}
+        };
+        var connection = new Connection(config);
+        connection.on('connect', function(err) {
+            console.log("Connected");
+            executeStatement();
 
-		pool.acquire((err, connection)=> {
-			if (err) {
-				console.error(err);
-				return;
-			}
-			var request = new Request(`SELECT [No_] as 'no' ,[Description] as 'desc' ,[Description 2] as 'desc2' FROM [${db}].[dbo].[${db}$Item] WHERE No_ = '${id}'` , (err, rowCount)=> {
-                if(err) throw err;
-                let data = rows[0];
-                res.render('share-with-db',{
-                    textDisplay: 'Please wait ...',
-                    url: url,
-                    urlRedirect : urlRedirect+id,
-                    urlImage : urlImage+id+'[0].png',
-                    titleProduct : data['desc']+' '+data['desc2'],
-                    descriptionProduct : 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry`s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+        });
+
+        var Request = require('tedious').Request;
+        var TYPES = require('tedious').TYPES;
+
+        function executeStatement() {
+            request = new Request(`SELECT [No_] as 'no' ,[Description] as 'desc' ,[Description 2] as 'desc2' FROM [${db}].[dbo].[${db}$Item] WHERE No_ = '${id}'`, function(err) {
+            if (err) {
+                console.log(err);} 
+            });
+            var result = "";
+            request.on('row', function(columns) {
+                columns.forEach(function(column) {
+                if (column.value === null) {
+                    console.log('NULL');
+                } else {
+                    result+= column.value + " ";
+                }
                 });
-				connection.release();
-			});
+                console.log(result);
+                result ="";
+            });
 
-			request.on('row', (columns)=> {
-				var row = {};
-				columns.forEach((column)=> {
-					row[column['metadata']['colName']] = column['value'];
-				});
-				rows.push(row);
-			});
-			connection.execSql(request);
-		});
+            request.on('done', function(rowCount, more) {
+            console.log(rowCount + ' rows returned');
+            });
+            connection.execSql(request);
+        }
+
+
+
+
+
+
+
+
+
+
+        // let db = 'SENTRALTUKANG';
+        // let id = req.params.id;
+		// let rows = [];
+
+		// pool.acquire((err, connection)=> {
+		// 	if (err) {
+		// 		console.error(err);
+		// 		return;
+		// 	}
+		// 	var request = new Request(`SELECT [No_] as 'no' ,[Description] as 'desc' ,[Description 2] as 'desc2' FROM [${db}].[dbo].[${db}$Item] WHERE No_ = '${id}'` , (err, rowCount)=> {
+        //         if(err) throw err;
+        //         let data = rows[0];
+        //         res.render('share-with-db',{
+        //             textDisplay: 'Please wait ...',
+        //             url: url,
+        //             urlRedirect : urlRedirect+id,
+        //             urlImage : urlImage+id+'[0].png',
+        //             titleProduct : data['desc']+' '+data['desc2'],
+        //             descriptionProduct : 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry`s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+        //         });
+		// 		connection.release();
+		// 	});
+
+		// 	request.on('row', (columns)=> {
+		// 		var row = {};
+		// 		columns.forEach((column)=> {
+		// 			row[column['metadata']['colName']] = column['value'];
+		// 		});
+		// 		rows.push(row);
+		// 	});
+		// 	connection.execSql(request);
+		// });
     }
 }
